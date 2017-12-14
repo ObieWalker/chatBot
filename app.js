10acd98e228adb231dc1388ef294aa3fc3e53573
ObieWalker <obinnawalker@gmail.com>
2017-12-14 10:30:05 +0100

    make changes to skype file

--- app.js
+++ app.js
@@ -1,94 +1,286 @@
+
+
 var restify = require('restify');
+//var builder = require('../../core/');
 var builder = require('botbuilder');
+
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+   console.log('I have %s listening to port %s', server.name, server.url); 
 });
+  
+var inMemoryStorage = new builder.MemoryBotStorage();
 
-// Create chat connector for communicating with the Bot Framework Service
+// Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-// Listen for messages from users 
+var bot = new builder.UniversalBot(connector).set('storage', inMemoryStorage); // Register in memory storage;
 server.post('/api/messages', connector.listen());
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, 
-    [
-        // function (session) {
-        // session.send("first test", session.message.text);    
-        // }
-        (session) => {
-            session.beginDialog('greetings', session.userData.profile);
-        },
-        (session) => {
-            session.beginDialog('enquiry', session.userData.enquiry);
-        },
-        (session, results) => {
-            const profile = session.userData.profile = results.response;
-            session.endConversation(`I hope I was able to answer your queries adequately. Goodbye ${profile.name}.`);
-        }
-        
-    ]
-);
 
-bot.dialog('greetings', [
-    (session, args, next) => {
-        session.dialogData.profile = args || {};
-        if (!session.dialogData.profile.name) {
-            builder.Prompts.text(session, `Hi! Before we begin, let me get to know you a bit. What is your name?`);
-        } else {
-            next();
+bot.on('conversationUpdate', function (message) {
+   // Check for group conversations
+    if (message.address.conversation.isGroup) {
+        // Send a hello message when bot is added
+        if (message.membersAdded) {
+            message.membersAdded.forEach(function (identity) {
+                if (identity.id === message.address.bot.id) {
+                    var reply = new builder.Message()
+                            .address(message.address)
+                            .text("Hello everyone!");
+                    bot.send(reply);
+                }
+            });
         }
+
+        // Send a goodbye message when bot is removed
+        if (message.membersRemoved) {
+            message.membersRemoved.forEach(function (identity) {
+                if (identity.id === message.address.bot.id) {
+                    var reply = new builder.Message()
+                        .address(message.address)
+                        .text("Goodbye");
+                    bot.send(reply);
+                }
+            });
+        }
+    }
+});
+
+bot.on('contactRelationUpdate', function (message) {
+    if (message.action === 'add') {
+        var name = message.user ? message.user.name : null;
+        var reply = new builder.Message()
+                .address(message.address)
+                .text("Oh hello %s... Thanks for adding me.", name || 'there');
+        bot.send(reply);
+    } else {
+        // delete their data
+    }
+});
+
+bot.on('deleteUserData', function (message) {
+    // User asked to delete their data
+});
+
+
+//=========================================================
+// Bots Middleware
+//=========================================================
+
+// Anytime the major version is incremented any existing conversations will be restarted.
+bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
+
+//=========================================================
+// Bots Global Actions
+//=========================================================
+
+bot.endConversationAction('goodbye', 'I hope I was able to answer your queries adequately. Goodbye', { matches: /^goodbye|bye|good bye/i});
+bot.beginDialogAction('help', '/help', { matches: /^help|help me|i need help/i });
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', [
+    function (session) {
+        // Send a greeting and show help.
+        var card = new builder.HeroCard(session)
+            .title("The VAIDS Bot")
+            .text("There to help, whenever")
+            .images([
+                 builder.CardImage.create(session, "http://oi66.tinypic.com/2z52dc3.jpg")
+            ]);
+        var msg = new builder.Message(session).attachments([card]);
+        session.send(msg);
+        session.send("Hi... I am the VAIDS bot for skype. I will answer your queries");
+        session.beginDialog('/help');
     },
-    (session, results, next) => {
-        if(results.response) {
-            if((results.response).match(/\Wis\W/)){
-                results.response = ((results.response).slice((results.response).indexOf('is')+2)).trim();
-            }
-            session.dialogData.profile.name = results.response;
-        }
-        if(!session.dialogData.profile.location) {
-            builder.Prompts.text(session, `Oh hello ${results.response}. What area are you chatting from?`)
-        } else {
-            next();
-        }
+    function (session, results) {
+        // Display menu
+        session.beginDialog('/menu');
     },
-    (session, results, next) => {
-        if(results.response) {
-            session.dialogData.profile.location = results.response;
-            if((results.response).toLowerCase().match(/\Wfrom\W/)){
-                results.response = ((results.response).slice((results.response).indexOf('from')+4)).trim();
-                 }
-            else if ((results.response).toLowerCase().match(/\Wis\W/)){
-                results.response = ((results.response).slice((results.response).indexOf('is')+2)).trim();
-            }
-            session.dialogData.profile.location = results.response;
-        }
-        if(!session.dialogData.profile.issue) {
-            builder.Prompts.text(session, `Ahhh ${results.response}. Anyway, I assume we are chatting because you want to know about vaids?`)
-        }
-        
-     },
-     (session, results) => {
-        session.endDialogWithResult({ response: session.dialogData.profile});
-     }
+    function (session, results) {
+        // Always say goodbye
+        session.send("Ok... See you later!");
+    }
 ]);
 
-bot.dialog('enquiry', [
-    (session, args, next) => {
-        session.dialogData.enquiry = args || {};
-        if (!session.dialogData.enquiry.enquiry) {
-            builder.Prompts.choice(session, "Please select the closest option", "Why Vaids|Vaids Essentials|Vaids Participation", { listStyle: 3});
+bot.dialog('/menu', [
+    function (session) {
+        //prompts|picture|cards|list|carousel|receipt|actions|(quit)
+        builder.Prompts.choice(session, "What general topic would you like to know about?", "Why Vaids?|Vaids Essentials|Vaids Participation|(quit)", { listStyle: 3});
+    },
+    function (session, results) {
+        if (results.response && results.response.entity != '(quit)') {
+            // Launch demo dialog
+            session.beginDialog('/' + results.response.entity);
         } else {
-            next();
+            // Exit the menu
+            session.endDialog();
         }
     },
-    (session, results) => {
-        session.endDialogWithResult({ response: session.dialogData.enquiry});
-     }
+    function (session, results) {
+        // The menu runs a loop until the user chooses to (quit).
+        session.replaceDialog('/menu');
+    }
+]).reloadAction('reloadMenu', null, { matches: /^menu|show menu/i });
+
+bot.dialog('/help', [
+    function (session) {
+        session.endDialog("If you type:\n\n* 'menu' - You will be given the main menu.\n* 'goodbye' - You will end this conversation.\n* 'help' - These commands will be displayed.");
+    }
 ]);
+
+bot.dialog('/Why Vaids?', [
+    function (session) {
+        builder.Prompts.choice(session, "Please select the closest option", "What is the Voluntary Asset & Income Declaration Scheme?|Why is the FG offering this scheme?|Why is the FG implementing VAIDS now?|Why aren't tax evaders persecuted?", { listStyle: 3});
+        //session.endDialog();
+    },
+    function (session, results) {
+        if((results.response) ==='What is the Voluntary Asset & Income Declaration Scheme?'){
+            session.send("You can pass a custom ");
+        }
+        session.endDialog('You can pass');
+    }    
+]);
+
+bot.dialog('/Vaids Essentials', [
+    function (session) {
+        builder.Prompts.choice(session, "Please select the closest option to your query", "How long will VAIDS last?|What are the benefits?|What types of Taxes will be covered?|Which period of default will be covered?|Who can participate in the scheme?|Can companies make declarations as well?|Where can one get information about VAIDS?", { listStyle: 3});
+    }
+]);
+
+bot.dialog('/Vaids Participation', [
+    function (session) {
+        builder.Prompts.choice(session, "Please select the closest option to your query", " How do I pay the tax due?|Pay all liabilities at once?|I don't know how much tax I owe?|My obligations be going forward as a result of my declaration?|I have no TIN and never paid tax?|FIRS and SBIRS invite people to participate?|Must I be resident in Nigeria to participate?|What if I spend time abroad?|For those that don't participate?|Pay tax on overseas income and assets?|Assets located outside of the country on which no income is earned?|Assets that are owned in no-tax jurisdictions?|Assets held in nominee names? Can they now be formally declared? Am I free to transfer them after the declaration?|The effect on waiver of interest and penalty introduced by the FIRS?|Will the tax authority review the information filed?|What about taxes I have already paid?|Can I declare anonymously?|Assurance that information provided  isn't be used against me?", { listStyle: 3});
+    }
+]);
+
+bot.dialog('/test', [
+    function (session) {
+        session.send("You can send the user a list of cards as multiple attachments in a single message...");
+    }
+]);
+
+bot.dialog('/carousel', [
+    function (session) {
+        session.send("You can pass a custom message to Prompts.choice() that will present the user with a carousel of cards to select from. Each card can even support multiple actions.");
+    },
+    function (session, results) {
+        var action, item;
+        var kvPair = results.response.entity.split(':');
+        switch (kvPair[0]) {
+            case 'select':
+                action = 'selected';
+                break;
+        }
+        switch (kvPair[1]) {
+            case '100':
+                item = "the <b>Space Needle</b>";
+                break;
+            case '101':
+                item = "<b>Pikes Place Market</b>";
+                break;
+            case '102':
+                item = "the <b>EMP Museum</b>";
+                break;
+        }
+        session.endDialog('You %s "%s"', action, item);
+    }    
+]);
+
+bot.dialog('/receipt', [
+    function (session) {
+        session.send("You can send a receipts for purchased good with both images and without...");
+        
+        // Send a receipt with images
+        var msg = new builder.Message(session)
+            .attachments([
+                new builder.ReceiptCard(session)
+                    .title("Recipient's Name")
+                    .items([
+                        builder.ReceiptItem.create(session, "$22.00", "EMP Museum").image(builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/a/a0/Night_Exterior_EMP.jpg")),
+                        builder.ReceiptItem.create(session, "$22.00", "Space Needle").image(builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/7/7c/Seattlenighttimequeenanne.jpg"))
+                    ])
+                    .facts([
+                        builder.Fact.create(session, "1234567898", "Order Number"),
+                        builder.Fact.create(session, "VISA 4076", "Payment Method"),
+                        builder.Fact.create(session, "WILLCALL", "Delivery Method")
+                    ])
+                    .tax("$4.40")
+                    .total("$48.40")
+            ]);
+        session.send(msg);
+
+        // Send a receipt without images
+        msg = new builder.Message(session)
+            .attachments([
+                new builder.ReceiptCard(session)
+                    .title("Recipient's Name")
+                    .items([
+                        builder.ReceiptItem.create(session, "$22.00", "EMP Museum"),
+                        builder.ReceiptItem.create(session, "$22.00", "Space Needle")
+                    ])
+                    .facts([
+                        builder.Fact.create(session, "1234567898", "Order Number"),
+                        builder.Fact.create(session, "VISA 4076", "Payment Method"),
+                        builder.Fact.create(session, "WILLCALL", "Delivery Method")
+                    ])
+                    .tax("$4.40")
+                    .total("$48.40")
+            ]);
+        session.endDialog(msg);
+    }
+]);
+
+bot.dialog('/signin', [ 
+    function (session) { 
+        // Send a signin 
+        var msg = new builder.Message(session) 
+            .attachments([ 
+                new builder.SigninCard(session) 
+                    .text("You must first signin to your account.") 
+                    .button("signin", "http://example.com/") 
+            ]); 
+        session.endDialog(msg); 
+    } 
+]); 
+
+
+bot.dialog('/actions', [
+    function (session) { 
+        session.send("Bots can register global actions, like the 'help' & 'goodbye' actions, that can respond to user input at any time. You can even bind actions to buttons on a card.");
+
+        var msg = new builder.Message(session)
+            .textFormat(builder.TextFormat.xml)
+            .attachments([
+                new builder.HeroCard(session)
+                    .title("Hero Card")
+                    .subtitle("Space Needle")
+                    .text("The <b>Space Needle</b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
+                    .images([
+                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
+                    ])
+                    .buttons([
+                        builder.CardAction.dialogAction(session, "weather", "Seattle, WA", "Current Weather")
+                    ])
+            ]);
+        session.send(msg);
+
+        session.endDialog("The 'Current Weather' button on the card above can be pressed at any time regardless of where the user is in the conversation with the bot. The bot can even show the weather after the conversation has ended.");
+    }
+]);
+
+// Create a dialog and bind it to a global action
+bot.dialog('/weather', [
+    function (session, args) {
+        session.endDialog("The weather in %s is 71 degrees and raining.", args.data);
+    }
+]);
+bot.beginDialogAction('weather', '/weather');   // <-- no 'matches' option means this can only be triggered by a button.
